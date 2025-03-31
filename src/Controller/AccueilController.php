5d4794b9b33581d36f3609d66ec7a30b7d43cd84
MattBaj-78 <matheo.bajard78@gmail.com>
2025-03-31 11:30:12 +0200

    Commit 5 Symfony du 31/03/2025

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -14,6 +14,10 @@ class AccueilController extends AbstractController
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig');
+        return $this->render('accueil/index.html.twig',
+        [
+            'lesNoms' => $noms,
+            'age' => $age
+        ]);
     }
 }
